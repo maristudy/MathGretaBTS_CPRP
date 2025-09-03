a8c2f2d74b9d350c9842822211e2e811eedf48e5
maristudy <mariannick.study@gmail.com>
2025-09-03 21:48:55 +0200

    premiers cours

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ for(let i=0;i<liste_h2.length;i++)
 		}
 	});
 }
+
+let liste_h3 = document.getElementsByTagName("h3");
+for(let i=0;i<liste_h3.length;i++)
+{
+	liste_h3[i].addEventListener("click",function () {
+		kids = this.closest("section").getElementsByTagName("article");
+		for (let j=0; j<kids.length; j++)
+		{
+			if (kids[j].classList.contains("visible"))
+			{
+				kids[j].classList.replace("visible", "cache");
+			}
+			else
+			{
+				kids[j].classList.replace("cache", "visible");
+			}
+		}
+	});
+}
