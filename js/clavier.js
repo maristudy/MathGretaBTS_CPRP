40c5cde84df786af7ecace19a40b1361cb6716d0
maristudy <mariannick.study@gmail.com>
2026-05-22 20:47:06 +0200

    clavier multilignes

--- js/clavier.js
+++ js/clavier.js
@@ -2,8 +2,12 @@ function clavier(tablo,idAffiche,idSaisie)
 {
 	affiche = document.getElementById(idAffiche);
 	saisie=document.getElementById(idSaisie)
+	saisie.setAttribute("class","saisie");
 	
+	ligne=document.createElement("p");
+	ligne.setAttribute("class","ligne");
 	backspace=document.createElement("p");
+	backspace.setAttribute("class","touche");
 	backspace.textContent="Backspace";
 	backspace.addEventListener("click",()=> 
 		{
@@ -12,24 +16,32 @@ function clavier(tablo,idAffiche,idSaisie)
 				saisie.textContent = saisie.textContent.substring(0,(saisie.textContent.length)-1);
 			}
 		})
-	affiche.append(backspace);
+	ligne.append(backspace);
 
 	efface=document.createElement("p");
+	efface.setAttribute("class","touche");
 	efface.textContent="Reset";
 	efface.addEventListener("click",()=> 
 		{
-			saisie.innerHTML = "&nbsp;";
+			saisie.innerHTML = "";
 		})
-	affiche.append(efface);
-	
-	tablo.forEach(elem => {
-		laTouche=document.createElement("p");
-		laTouche.textContent=elem;
-		laTouche.addEventListener("click",()=> 
-		{
-			saisie.textContent = saisie.textContent+elem
+	ligne.append(efface);
+		affiche.append(ligne);
+		
+	tablo.forEach(tab => {
+		ligne=document.createElement("p");
+		ligne.setAttribute("class","ligne");
+		tab.forEach(elem => {
+			laTouche=document.createElement("p");
+			laTouche.setAttribute("class","touche");
+			laTouche.textContent=elem;
+			laTouche.addEventListener("click",()=> 
+			{
+				saisie.textContent = saisie.textContent+elem
+			})
+			ligne.append(laTouche);
 		})
-		affiche.append(laTouche);
+		affiche.append(ligne);
 	})
 
 		
