5674c1d5bb6f212f2e43e886eeaa419f69e05ab9
maristudy <mariannick.study@gmail.com>
2026-05-23 09:14:00 +0200

    clavier test ok

--- js/clavier.js
+++ js/clavier.js
@@ -1,7 +1,6 @@
 function clavier(tablo,idAffiche,idSaisie)
 {
 	affiche = document.getElementById(idAffiche);
-	alert(affiche.innerHTML.length);
 	if(affiche.innerHTML.length > 5)
 	{
 		affiche.innerHTML="";
